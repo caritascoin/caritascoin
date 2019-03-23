8e01571b91780d4f74178755fe3f3c9dafff4da8
hernss <hernantravado@yahoo.com.ar>
2019-03-22 23:16:52 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4424,11 +4424,11 @@ bool CheckBlockHeader(const CBlockHeader& block, CValidationState& state, bool f
             REJECT_INVALID, "high-hash");
 
     // Version 4 header must be used after Params().Zerocoin_StartHeight(). And never before.
-    if (block.GetBlockTime() > Params().Zerocoin_StartTime()) {
+    /*if (block.GetBlockTime() > Params().Zerocoin_StartTime()) {
         if (block.nVersion < Params().Zerocoin_HeaderVersion())
             return state.DoS(50, error("CheckBlockHeader() : block version must be above 4 after ZerocoinStartHeight"),
                 REJECT_INVALID, "block-version");
-    } /*else {
+    } else {
         if (block.nVersion >= Params().Zerocoin_HeaderVersion())
             return state.DoS(50, error("CheckBlockHeader() : block version must be below 4 before ZerocoinStartHeight"),
                 REJECT_INVALID, "block-version");
